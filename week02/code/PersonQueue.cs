c799f608ed38f2f2e6416a05707ed219b42057f8
thankgodandrew1 <and22046@byui.edu>
2024-06-25 23:32:14 -0600

    fixed bug in Person.Queuecs

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -11,10 +11,12 @@ public class PersonQueue {
     /// </summary>
     /// <param name="person">The person to add</param>
     public void Enqueue(Person person) {
-        _queue.Insert(0, person);
+        // _queue.Insert(0, person); //breaks the FIFO data structure, tries to add to the start of the list instead of end of the list
+        _queue.Add(person); // Adds to the end or back
     }
 
     public Person Dequeue() {
+        if (_queue.Count == 0) throw new InvalidOperationException("The queue is empty."); // addedan exception in case queuee is empty to end gracefully
         var person = _queue[0];
         _queue.RemoveAt(0);
         return person;
