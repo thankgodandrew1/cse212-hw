0ea330e144cd6a04fd0d8953d4020960e3d53391
thankgodandrew1 <and22046@byui.edu>
2024-07-21 04:05:37 -0600

    Wee 06: Trees

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -66,6 +66,12 @@ public class BinarySearchTree : IEnumerable<int> {
 
     private void TraverseBackward(Node? node, List<int> values) {
         // TODO Problem 3
+        // # to solve this i traversed the right subtree first, then the current node, finally the left subtrre
+        if (node is not null) {
+            TraverseBackward(node.Right, values);
+            values.Add(node.Data);
+            TraverseBackward(node.Left, values);
+        }
     }
 
     /// <summary>
--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,22 +1,29 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
-
-    public void Insert(int value) {
-        if (value < Data) {
+    // # Problem 1
+    // how do i insert unique value only?
+    // if value == Data, do nothing (no duplicates would be allowed)
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
-            // Insert to the right
+        // Insert to the right 
+        else if (value > Data)
+        {
             if (Right is null)
                 Right = new Node(value);
             else
@@ -24,13 +31,24 @@ public class Node {
         }
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
         // TODO Start Problem 2
-        return false;
+        //i'd use recursion to search the tree for the specifie value
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // To solve -- find the maximum height of the left and right subtrees
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight); // Replace this line with the correct return statement(s)
     }
 }
